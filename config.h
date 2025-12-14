d8e35e987262bc332ce28ab9e72d85ea475a1d28
H2OGabriel <gabriel.hoo@edu.ece.fr>
2025-12-14 18:06:20 +0100

    Update config.h

--- config.h
+++ config.h
@@ -4,29 +4,29 @@
 #include <Arduino.h>
 
 // --- CONSTANTES ---
-#define MAX_MESSAGE_LEN 120
-#define PSEUDO_LEN 6          
-#define PACKET_DATA_SIZE 22   
+#define MaxMessageLen 120
+#define PseudoLen 6          
+#define PacketDataSize 22   
 
 // --- ADRESSES EEPROM ---
-#define ADDR_MARKER   0   
-#define ADDR_CHANNEL  1   
-#define ADDR_SLOT     2   
-#define ADDR_SOUND    3   
-#define ADDR_PSEUDO   10  
-#define MAGIC_BYTE    101 // Changé
+#define AddrMarker   0   
+#define AddrCannel  1   
+#define AddrSlot     2   
+#define AddrSound    3   
+#define AddrPseudo   10  
+#define MagicByte    101 // Changé
 
 // --- PINS ---
 #define PIN_CE 7
 #define PIN_CSN 8
-#define PIN_BUZZER 10
-#define PIN_RED 5
-#define PIN_GREEN 6
-#define PIN_BLUE 9
-#define ENC_CLK 3       
+#define BuzzerPin 10
+#define RedPin 5
+#define GreenPin 6
+#define BluePin 9
+#define EncCLK 3       
 #define ENC_DT 4       
-#define ENC_SW 2       
-#define BTN_SEND A6    
+#define NCSW 2       
+#define senBtn A6    
 
 // --- ECRAN ---
 #define SCREEN_WIDTH 128
@@ -34,13 +34,13 @@
 #define OLED_RESET -1 
 
 // --- ENUMS & STRUCTS ---
-#define PRIO_FAIBLE 0 
-#define PRIO_MOYEN  1 
-#define PRIO_HAUTE  2 
+#define LowPrio 0 
+#define AveragePrio  1 
+#define HighPrio  2 
 
-#define SON_CLASSIQUE 0
-#define SON_DOUBLE    1
-#define SON_CONTINU   2
+#define ClassicSound 0
+#define DoubleSound    1
+#define EndlessSound   2
 
 struct Packet {
   byte msgId;         
@@ -52,15 +52,15 @@ struct Packet {
 };
 
 enum Mode {
-  MODE_MENU_PRINCIPAL,    
-  MODE_MENU_REGLAGE,      
-  MODE_EDIT_PSEUDO,       
-  MODE_EDIT_CANAL,
-  MODE_EDIT_SLOT,
-  MODE_EDIT_SOUND,        
-  MODE_ECRITURE,          
-  MODE_CHOIX_PRIORITE,    
-  MODE_ALERTE_RECU        
+  MainMenu,    
+  SettingsMenu,      
+  PseudoMode,       
+  CanalEditMode,
+  SlotEditMode,
+  SoundEditMode,        
+  WritingMode,          
+  PriorityMode,    
+  AlarmMode        
 };
 
 #endif
